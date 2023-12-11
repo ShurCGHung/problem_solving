785d4dc1a6a5c2e766d4f54610f6b96746b868f9
Hưng Cao <caogiahung2501@gmail.com>
2023-12-11 10:46:59 +0700

    (!) Update filename to match template

---
+++ ProjectEuler/problem_003.py
@@ -0,0 +1,31 @@
+"""
+Largest Prime Factor
+"""
+import math
+import time
+
+def largestPrimeFactor(number: int) -> int:
+    """Optimized solution"""
+    largest_factor = 0
+
+    # Handling even numbers
+    while number % 2 == 0:
+        largest_factor = 2
+        number //= 2
+
+    # Finding other prime factors
+    for i in range(3, int(math.sqrt(number)) + 1, 2):
+        while number % i == 0:
+            largest_factor = i
+            number //= i
+
+    # If the remaining number is a prime greater than 2
+    if number > 2:
+        largest_factor = number
+
+    return largest_factor
+
+if __name__ == "__main__":
+    start_time = time.time()
+    print(largestPrimeFactor(number=60085147514325910))
+    print("--- %.10f seconds ---" % (time.time() - start_time))
